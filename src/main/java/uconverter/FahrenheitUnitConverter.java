9b75107bd5d32ce58ee84305642bb3c93e0a30f3
DmitryPestr <dmitry.net@inbox.lv>
2017-04-26 00:31:26 +0300

    new Tem - formula

--- src/main/java/uconverter/FahrenheitUnitConverter.java
+++ src/main/java/uconverter/FahrenheitUnitConverter.java
@@ -4,12 +4,12 @@ public class FahrenheitUnitConverter extends UnitConverter {
 
     @Override
     public double toSI(double value) {
-        return (value + 459.67) * 9.0 / 5.0;
+        return (value - 32) * 5.0 / 9.0 + 273.15;
     }
 
     @Override
     public double fromSI(double siValue) {
-        return siValue * 9.0/5.0 - 459.67;
+        return (siValue - 273.15) * 9.0 / 5.0 + 32;
     }
 
     @Override
