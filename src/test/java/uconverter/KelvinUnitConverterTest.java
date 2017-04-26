88549f5fb22fa63da01b83be7a400ccaf82b74df
JavaCourses <dmitry.net@inbox.lv>
2017-04-26 20:15:58 +0300

    new Test

--- src/test/java/uconverter/KelvinUnitConverterTest.java
+++ src/test/java/uconverter/KelvinUnitConverterTest.java
@@ -1,14 +1,30 @@
 package uconverter;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by JavaCourses on 26.04.2017..
  */
+
+@RunWith(Parameterized.class)
 public class KelvinUnitConverterTest {
     KelvinUnitConverter converter = new KelvinUnitConverter();
+
+    @Parameterized.Parameters
+    public Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {10.0, 10.0},
+                {100.0, 100.0},
+                {150.0, 150.0}
+        });
+    }
 
     @Test
     public void toSI() throws Exception {
